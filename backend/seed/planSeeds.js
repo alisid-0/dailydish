d2dbcb7c1d7c27c9206cb65aae982f75e3574b2a
Bryan <b.velez205@gmail.com>
2023-06-30 11:28:09 -0400

    Seeding finished

--- backend/seed/planSeeds.js
+++ backend/seed/planSeeds.js
@@ -5,57 +5,57 @@ const main = async() => {
 
     const plan1 = new Plan({
         name: 'General Plan',
-        price: 49.99,
-        measPerWeek: 5,
-        mealType: ''
+        pricePerMeal: 1.25,
+        mealsPerWeek: '',
+        meals: ['']
     })
     await plan1.save()
 
     const plan2 = new Plan({
         name: 'Halal Plan',
-        price: 79.99,
-        measPerWeek: 7,
-        mealType:''
+        pricePerMeal: 2.99,
+        mealsPerWeek: '',
+        meals: ['']
     })
     await plan2.save()
 
     const plan3 = new Plan({
         name: 'Vegan Plan',
-        price: 99.99,
-        measPerWeek: 10,
-        mealType: ''
+        pricePerMeal: 1.50,
+        mealsPerWeek: '',
+        meals: ['']
     })
     await plan3.save()
 
     const plan4 = new Plan({
         name: 'Kosher Plan',
-        price: 99.99,
-        measPerWeek: 10,
-        mealType: ''
+        pricePerMeal: 2.99,
+        mealsPerWeek: '',
+        meals: ['']
     })
     await plan4.save()
 
     const plan5 = new Plan({
         name: 'Vegatarian Plan',
-        price: 99.99,
-        measPerWeek: 10,
-        mealType: ''
+        pricePerMeal: 1.00,
+        mealsPerWeek: '',
+        meals: ['']
     })
     await plan5.save()
 
     const plan6 = new Plan({
         name: 'Pescatarian Plan',
-        price: 99.99,
-        measPerWeek: 10,
-        mealType: ''
+        pricePerMeal: 2.00,
+        mealsPerWeek: '',
+        meals: ['']
     })
     await plan6.save()
 
     const plan7 = new Plan({
         name: 'Gluten-Free Plan',
-        price: 99.99,
-        measPerWeek: 10,
-        mealType: ''
+        pricePerMeal: 2.00,
+        mealsPerWeek: '',
+        meals: ['']
     })
     await plan7.save()
 }
