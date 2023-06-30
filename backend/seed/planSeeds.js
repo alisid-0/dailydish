bd5f1b6357bb9cc843409acff4442ad1f2ac0a1f
Bryan <b.velez205@gmail.com>
2023-06-30 11:17:28 -0400

    bryan

--- backend/seed/planSeeds.js
+++ backend/seed/planSeeds.js
@@ -4,7 +4,7 @@ const { Plan } = require("../models")
 const main = async() => {   
 
     const plan1 = new Plan({
-        name: 'Basic Plan',
+        name: 'General Plan',
         price: 49.99,
         measPerWeek: 5,
         mealType: ''
@@ -12,7 +12,7 @@ const main = async() => {
     await plan1.save()
 
     const plan2 = new Plan({
-        name: 'Premium Plan',
+        name: 'Halal Plan',
         price: 79.99,
         measPerWeek: 7,
         mealType:''
@@ -20,12 +20,44 @@ const main = async() => {
     await plan2.save()
 
     const plan3 = new Plan({
-        name: 'Deluxe Plan',
+        name: 'Vegan Plan',
         price: 99.99,
         measPerWeek: 10,
         mealType: ''
     })
     await plan3.save()
+
+    const plan4 = new Plan({
+        name: 'Kosher Plan',
+        price: 99.99,
+        measPerWeek: 10,
+        mealType: ''
+    })
+    await plan4.save()
+
+    const plan5 = new Plan({
+        name: 'Vegatarian Plan',
+        price: 99.99,
+        measPerWeek: 10,
+        mealType: ''
+    })
+    await plan5.save()
+
+    const plan6 = new Plan({
+        name: 'Pescatarian Plan',
+        price: 99.99,
+        measPerWeek: 10,
+        mealType: ''
+    })
+    await plan6.save()
+
+    const plan7 = new Plan({
+        name: 'Gluten-Free Plan',
+        price: 99.99,
+        measPerWeek: 10,
+        mealType: ''
+    })
+    await plan7.save()
 }
 
 const run = async() => {
