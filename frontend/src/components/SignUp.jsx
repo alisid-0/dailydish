af13e015235e7bcd070952017a309522b88043dc
Ali Siddiqui <alisiddiquia22@yahoo.com>
2023-07-03 12:12:58 -0500

    comitting unfinished changes for pull

--- frontend/src/components/SignUp.jsx
+++ frontend/src/components/SignUp.jsx
@@ -1,4 +1,4 @@
-import {Container, Form, Button} from 'react-bootstrap'
+import {Container, Form, Button, Alert, InputGroup, Row, Col} from 'react-bootstrap'
 import React, { useEffect, useState, useContext } from 'react'
 import { LoginContext } from '../App'
 import { Link } from 'react-router-dom'
@@ -6,22 +6,45 @@ import axios from 'axios'
 import bcrypt from 'bcryptjs'
 
 function SignUp(){
+
+    const [message, setMessage] = useState(null)
+    const [variant, setVariant] = useState(null)
+
+    const [validated, setValidated] = useState(false);
+
+    const handleSubmit = (event) => {
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+        event.preventDefault();
+        event.stopPropagation();
+    }
+
+    setValidated(true);
+    };
+
     const signUpHandler = async()=>{
         const email = document.getElementById('formBasicEmail').value
         const passwordVal = document.getElementById('formBasicPassword').value
+        const confirmPasswordVal = document.getElementById(`formBasicConfirmPassword`).value
         const username = document.getElementById('formBasicName').value
         const usersApi = await axios.get('http://localhost:3001/api/users')
         const users = usersApi.data
         const emailInUse = users.some(user => user.email === email)
         
         const password = bcrypt.hashSync(passwordVal,10)
-        
-
 
         if(emailInUse){
-            alert('Email is already in use')
-        } else if (password.length < 8){
-            alert(`Password must be 8 characters or greater.`)
+            setVariant('danger')
+            setMessage('Email is already in use.')
+        } else if (email.length < 3){
+            setVariant(`danger`)
+            setMessage(`Must be a valid email.`)
+        } else if(passwordVal.length < 8){
+            setVariant(`danger`)
+            setMessage('Password must be 8 characters in length or greater.')
+        } else if (passwordVal != confirmPasswordVal){
+            setVariant(`danger`)
+            setMessage(`Passwords do not match.`)
         } else {
             const newUser = {
                 username,
@@ -30,14 +53,16 @@ function SignUp(){
             }
 
             await axios.post('http://localhost:3001/api/users', newUser)
-            alert('User created successfully')
+            setVariant('info')
+            setMessage(`User created successfully!`)
         }
     }
 
     return(
-        <Container className='text-light bg-dark p-5 mt-5' style={{maxWidth:`30vw`, borderRadius:`3rem`}}>
+        <Container className='text-light bg-dark p-5 mt-5' style={{maxWidth:`40vw`, borderRadius:`3rem`}}>
             <h1 className='py-5'>Sign Up</h1>
             <Form>
+                {message && (<Alert variant={variant}>{message}</Alert>)}
                 <Form.Group className="mb-3" controlId='formBasicName'>
                     <Form.Label>Name</Form.Label>
                     <Form.Control type='username' placeholder='Enter Name'/>
@@ -49,10 +74,28 @@ function SignUp(){
                 <Form.Group className="mb-3" controlId='formBasicPassword'>
                     <Form.Label>Password</Form.Label>
                     <Form.Control type='password' placeholder='Enter Password'/>
-                    <Form.Text className='text-dark'>We'll never share your password with anyone else.</Form.Text>
+                </Form.Group><Form.Group className="mb-3" controlId='formBasicConfirmPassword'>
+                    <Form.Label>Confirm Password</Form.Label>
+                    <Form.Control type='password' placeholder='Enter Password'/>
+                    <Form.Text className='text-light'>We'll never share your password with anyone else.</Form.Text>
                 </Form.Group>
             </Form>
+            <Form noValidate validated={validated} onSubmit={handleSubmit}>
+                <Form.Group controlId="validationCustom01">
+                    <Form.Label>Name</Form.Label>
+                    <Form.Control placeholder="Enter Name"/>
+                    <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+                </Form.Group>
+                <Form.Group controlId="validationCustomUsername">
+                    <Form.Label>Password</Form.Label>
+                        <Form.Control type="password" placeholder="Password" required/>
+                        <Form.Control.Feedback type="invalid">Please choose a password.</Form.Control.Feedback>                    
+                </Form.Group>
+                
+                <Button type="submit">Submit form</Button>
+            </Form>
             <Button onClick={signUpHandler}>Sign Up</Button>
+            <p className="pt-5"><i>Already have an account? <a href='/login'as={Link} to='/login'>Log In.</a></i></p>
         </Container>
     )
 }
