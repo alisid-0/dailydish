3c8ab2974989e5441fc3c338fc734aeaa04a2216
Ali Siddiqui <alisiddiquia22@yahoo.com>
2023-07-04 19:52:51 -0500

    commit for pulling and deletion

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+
 const express = require(`express`)
 const routes = require(`./routes/AppRouter`)
 const db = require(`./db`)
@@ -6,17 +8,37 @@ const cors = require(`cors`)
 
 const PORT = process.env.PORT || 3001
 const app = express()
+const stripe = require('stripe')(process.env.STRIPE_PRIVATE_KEY)
+
+const storeItems = new Map(
+    [
+        [1, {priceInCents: 10000, name: `Learn React Today`}],
+        [2, {priceInCents: 20000, name: 'Learn CSS Today'}]
+    ]
+)
 
 app.use(express.json())
 app.use(logger(`dev`))
 app.use(cors())
 
 // app.use(express.static(`./vite-app/dist`))
-app.use(express.static(`./frontend/src`))
+app.use(express.static(`../frontend/dist`))
+
+app.post('/create-checkout-session', async (req,res) =>{
+    try{
+        const session = await stripe.checkoutsessions.create({
+            payment_method_types: ['card'],
+            mode: 'subscription'
+        })
+    } catch( e){
+        res.status(500).json({error: e.message})
+    }
+    res.json({ url: `Hi`})
+})
 
 app.use(`/api`, routes)
 
 // app.use(`*`, express.static(`./vite-app/dist`))
-app.use(`*`, express.static(`./frontend/src`))
+app.use(`*`, express.static(`../frontend/dist`))
 
 app.listen(PORT, ()=> console.log(`Listening on port: ${PORT}`))
