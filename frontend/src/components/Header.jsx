103b1841aedf315a817313b759bbaa639d7e86cb
ssstumbles <sara.rosic@gmail.com>
2023-07-07 00:45:19 -0400

    homepage styling done

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -11,10 +11,10 @@ function Header(){
 
     return(
         
-        <Navbar expand="lg" className='py-3'>
+        <Navbar classexpand="lg" className='py-3' style={{backgroundColor: `#D2D0BA`}} >
             <Container>
-                <Navbar.Brand href="/">DailyDish</Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Brand className='fs-3' style={{color:`#5E747F`}} href="/">DailyDish</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav"/>
                 <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="me-auto justify-content-between" style={{width:`100%`}}>
                     <Nav.Link href="/">Home</Nav.Link>
@@ -22,7 +22,7 @@ function Header(){
                     <Nav.Link href='/checkout'>Checkout</Nav.Link>
                     
                     <div style={{flexGrow: 1, display: 'flex', justifyContent: 'flex-end'}}>
-                        <Button className='login-button' href='/login'>
+                        <Button className='login-button' href='/login' style={{backgroundColor:`#B6BE9C`, color:`#5E747F `, border:`none`}}>
                             {signedIn ? ('Account') : ('Log In')}
                         </Button>
                     </div>
--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -19,39 +19,39 @@ function Home(){
 
     return(
         <>
-            <div className='home-page'>
-                <div className='home'>
+            {/* <div className='py-1 home-page'>
+                <div className='py-1 home'>
                     <h1 style={{fontSize:`8vw`}}>DailyDish</h1>
                     <p className='mx-2' style={{fontSize:`2vw`}}>Dine Different.</p>
-                    <Button className='mx-2 btn-primary'>Get Started</Button>
+                    <Button className='mx-2' style={{backgroundColor:`#5E747F`, color:`#E5C1BD`}}>Get Started</Button>
                 </div>
-            </div>
-            <div className="py-5 home-background" >
-
-                <div className='py-5 intro'>
-                    <h1 className='py-5 text-light'>Welcome to <span className="blue">DailyDish.</span></h1>
-                    <h4 className='py-3 text-light'>Your ultimate destination for delicious and convenient meal solutions!</h4>
-                    <h4 className='py-3 px-5 text-light'>At DailyDish, we understand the challenges of planning and preparing meals amidst busy schedules and demanding lifestyles. That's why we've created a service that takes the hassle out of cooking while delivering incredible flavors and wholesome ingredients right to your doorstep.</h4>
-                    <h4 className='py-3 px-5 text-light'>With DailyDish, you'll have access to a diverse and ever-growing menu of chef-curated meals designed to tantalize your taste buds. Our team of culinary experts carefully crafts each recipe to ensure a perfect balance of flavors, textures, and nutritional value. No matter what your budget, dietary needs or family size. Our flexible meal kits come complete with everything you need to quickly cook your meal. Whether you're a busy professional, a health-conscious individual, or a family looking to enjoy quality time over a delicious meal, we have you covered. Choose from a variety of dietary options, including vegetarian, vegan, gluten-free, kosher, and more. We believe that everyone deserves a meal that suits their unique preferences and requirements. Not only do we prioritize convenience and flavor, but we also value your health and well-being. Our commitment to using high-quality, locally sourced ingredients ensures that every bite you take is a step towards a healthier lifestyle. You can trust DailyDish to provide you with nourishing meals that support your overall well-being.</h4>
-                    <h4 className='py-3 px-5 text-light'>Join us today and experience the joy of effortless cooking and extraordinary flavors. Get ready to explore a world of culinary delights, elevate your dining experience, and create lasting memories around the dinner table. We're here to make your life easier, tastier, and more enjoyable.</h4>
-                    <Button> Get started in three easy steps</Button>
+            </div> */}
+            <div className="mx-auto home-background" >
+            <Container className='me-0' style={{width:`75%`}} >
+                <div className='py-5 intro' style={{textAlign:`left`}}>
+                    <h2 className='py-5' style={{textAlign:`center`}} >Welcome to <span className="highlight">DailyDish.</span></h2>
+                    <h4 className='py-3t'>Your ultimate destination for delicious and convenient meal solutions!</h4>
+                    <h4 className='py-3 px-5'>At DailyDish, we understand the challenges of planning and preparing meals amidst busy schedules and demanding lifestyles. That's why we've created a service that takes the hassle out of cooking while delivering incredible flavors and wholesome ingredients right to your doorstep.</h4>
+                    <h4 className='py-3 px-5t'>With DailyDish, you'll have access to a diverse and ever-growing menu of chef-curated meals designed to tantalize your taste buds. Our team of culinary experts carefully crafts each recipe to ensure a perfect balance of flavors, textures, and nutritional value. No matter what your budget, dietary needs or family size. Our flexible meal kits come complete with everything you need to quickly cook your meal. Whether you're a busy professional, a health-conscious individual, or a family looking to enjoy quality time over a delicious meal, we have you covered. Choose from a variety of dietary options, including vegetarian, vegan, gluten-free, kosher, and more. We believe that everyone deserves a meal that suits their unique preferences and requirements. Not only do we prioritize convenience and flavor, but we also value your health and well-being. Our commitment to using high-quality, locally sourced ingredients ensures that every bite you take is a step towards a healthier lifestyle. You can trust DailyDish to provide you with nourishing meals that support your overall well-being.</h4>
+                    <h4 className='py-3 px-5'>Join us today and experience the joy of effortless cooking and extraordinary flavors. Get ready to explore a world of culinary delights, elevate your dining experience, and create lasting memories around the dinner table. We're here to make your life easier, tastier, and more enjoyable.</h4>
+                    <Button style={{backgroundColor:`#5E747F`, color:`#E5C1BD`, width:`100%`}}> Get started in these easy steps</Button>
                 </div>
-
-                <h1 className='py-5 text-light'>Why Us?</h1>
+                </Container>
+                <h2 className='py-5'>Why Us?</h2>
                 <Container>
                     <Row>
                         <Col>
                             <img className='home-img' src={meals.length > 0 && meals[2].imageUrl}></img>
-                            <h3 className='text-light'>Affordable</h3>
+                            <h3>Affordable</h3>
                         </Col>
                         <Col>
                             <img className='home-img' src={meals.length > 0 && meals[5].imageUrl}></img>
-                            <h3 className='text-light'>Healthy</h3>
+                            <h3>Healthy</h3>
                             
                         </Col>
                         <Col>
                             <img className='home-img' src={meals.length > 0 && meals[4].imageUrl}></img>
-                            <h3 className='text-light'>Different</h3>
+                            <h3>Different</h3>
                         </Col>
                     </Row>
                 </Container>
